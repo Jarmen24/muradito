a49d4c0522e42cfebe743368496416c201c2eafb
Jarmen Cachero <cachero.jarmen.bscs2022@gmail.com>
2026-04-15 22:59:48 +0800

    feat: new header

---
+++ components/Layout/Header.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+
+const Header = () => {
+  return (
+    <header className="flex items-center w-full px-6 py-2 border-4 border-red-600 bg-white">
+      {/* 1. Logo stays on the left */}
+      <div className="shrink-0">
+        <Image src="/bookit.png" width={80} height={20} alt="logo" />
+      </div>
+
+      {/* 2. Links - "mx-auto" pushes everything else away to center these */}
+      <nav className="flex items-center gap-7 mx-auto">
+        <Link href="/" className="text-xl font-bold">
+          Home
+        </Link>
+        <Link href="/" className="text-xl font-bold">
+          Hotel & Homes
+        </Link>
+        <Link href="/" className="text-xl font-bold">
+          Real Estate
+        </Link>
+        <Link href="/" className="text-xl font-bold">
+          Articles
+        </Link>
+      </nav>
+
+      {/* 3. Button - stays on the right */}
+      <div className="shrink-0">
+        <Button>Get Started</Button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
