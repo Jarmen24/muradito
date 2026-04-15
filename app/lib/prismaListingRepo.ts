12fa7bb256446b9d8c6692c71804c039a2d5eb78
Jarmen Cachero <cachero.jarmen.bscs2022@gmail.com>
2026-04-16 00:57:12 +0800

    feat: listing repo for prisma

---
+++ app/lib/prismaListingRepo.ts
@@ -0,0 +1,8 @@
+import prisma from "./db";
+import { ListingRepo } from "./interfaces/ListingRepo";
+
+export const prismaListingRepo: ListingRepo = {
+    getAllListings: async () => {
+        return await prisma.listing.findMany();
+    }
+}
