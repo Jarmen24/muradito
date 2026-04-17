a15541e7badb96f2c3a3d4c2e5b352deb1da7edf
Jarmen Cachero <cachero.jarmen.bscs2022@gmail.com>
2026-04-17 23:44:45 +0800

    add:  getFirstFive function

--- app/lib/prismaListingRepo.ts
+++ app/lib/prismaListingRepo.ts
@@ -2,7 +2,12 @@ import prisma from "./db";
 import { ListingRepo } from "./interfaces/ListingRepo";
 
 export const prismaListingRepo: ListingRepo = {
-    getAllListings: async () => {
-        return await prisma.listing.findMany();
-    }
-}
+  getAllListings: async () => {
+    return await prisma.listing.findMany();
+  },
+  getFirstFive: async () => {
+    return await prisma.listing.findMany({
+      take: 5,
+    });
+  },
+};
