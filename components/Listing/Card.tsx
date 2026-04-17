9cfce0f7c1d578ed1d5bbc2f6b277fce9e53a5ae
Jarmen Cachero <cachero.jarmen.bscs2022@gmail.com>
2026-04-17 23:45:15 +0800

    fix: image now has fixed height

--- components/Listing/Card.tsx
+++ components/Listing/Card.tsx
@@ -1,35 +1,41 @@
-import { StarIcon } from 'lucide-react'
-import Image from 'next/image'
-import React from 'react'
+import { StarIcon } from "lucide-react";
+import Image from "next/image";
+import React from "react";
 
-const Card = ({ src, title, price }: { src: string, title: string, price: string }) => {
+const Card = ({
+  src,
+  title,
+  price,
+}: {
+  src: string;
+  title: string;
+  price: string;
+}) => {
   return (
     <div className="h-full rounded-2xl border border-gray-200 bg-white">
-            <Image
-              src={src}
-              className="object-cover w-full rounded-t-xl"
-              width={200}
-              height={300}
-              alt="logo"
-            />
-            <div className="py-2 px-4">
-              <div className="flex items-center justify-between">
-              <h4 className="text-sm font-semibold">{title}</h4>
-              <span className="flex items-center">
-                <StarIcon className="w-4 h-4 text-yellow-500" fill="currentColor"/>
-                <StarIcon className="w-4 h-4 text-yellow-500" fill="currentColor"/>
-                <StarIcon className="w-4 h-4 text-yellow-500" fill="currentColor"/>
-                <StarIcon className="w-4 h-4 text-yellow-500" />
-                <StarIcon className="w-4 h-4 text-yellow-500" />
+      <div className="w-full relative h-32">
+        <Image
+          src={src}
+          className="object-cover w-full rounded-t-xl"
+          fill
+          alt="logo"
+        />
+      </div>
+      <div className="py-2 px-4">
+        <div className="flex items-center justify-between">
+          <h4 className="text-xs font-semibold">{title}</h4>
+        </div>
+        <p className="text-xs text-gray-500">{price} / night</p>
+        <span className="flex items-center">
+          <StarIcon className="w-3 h-3 text-yellow-500" fill="currentColor" />
+          <StarIcon className="w-3 h-3 text-yellow-500" fill="currentColor" />
+          <StarIcon className="w-3 h-3 text-yellow-500" fill="currentColor" />
+          <StarIcon className="w-3 h-3 text-yellow-500" />
+          <StarIcon className="w-3 h-3 text-yellow-500" />
+        </span>
+      </div>
+    </div>
+  );
+};
 
-              </span>
-              </div>
-              <p className="text-xs text-gray-500">
-              {price} / night
-              </p>
-            </div>
-          </div>
-  )
-}
-
-export default Card
+export default Card;
