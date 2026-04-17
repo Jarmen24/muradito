80e49786bd0c6efdc3889789c9fa0be63a9a49b4
Jarmen Cachero <cachero.jarmen.bscs2022@gmail.com>
2026-04-17 23:45:46 +0800

    chore: fetch only first five and added a link on each card

--- app/listing/page.tsx
+++ app/listing/page.tsx
@@ -4,19 +4,26 @@ import { StarIcon } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 import { Listing } from "@prisma/client";
-import { getAllListings } from "@/app/lib/listing";
+import { getFirstFive } from "@/app/lib/listing";
+import Link from "next/link";
 
 const AllListing = async () => {
-  const listings: Listing[] = await getAllListings();
+  const listings: Listing[] = await getFirstFive();
   if (!listings.length) return <div>No listings found</div>;
   return (
     <div className="min-h-screen bg-gray-50 p-6">
       <div className="max-w-5xl mx-auto space-y-6">
         <GridContainer>
           {listings.map((listing) => (
-            <Card key={listing.id} src={listing.images[0]} title={listing.name} price={`₱${listing.price}`} />
+            <Link key={listing.id} href={`/listings/${listing.id}`}>
+              <Card
+                key={listing.id}
+                src={listing.images[0]}
+                title={listing.name}
+                price={`₱${listing.price}`}
+              />
+            </Link>
           ))}
-          
         </GridContainer>
       </div>
     </div>
