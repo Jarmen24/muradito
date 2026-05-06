409cbac9030786e83008991d23c2a13775a3cba4
Jarmen Cachero <cachero.jarmen.bscs2022@gmail.com>
2026-05-07 00:22:28 +0800

    refacto: logo and profile logo and nav links

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -5,36 +5,44 @@ import { Button } from "../ui/button";
 import { getServerSession } from "next-auth";
 import options from "@/app/api/auth/[...nextauth]/options";
 import LogoutButton from "./LogoutButton";
+import { DropdownMenuAvatar } from "./DropdownMenuAvatar";
 
 const Header = async () => {
+  const navItem =
+    "text-md px-4 py-3 rounded-xl transition duration-300 ease-in-out";
+
   const session = await getServerSession(options);
   console.log(session);
   return (
     <header className="flex items-center w-full px-6 py-2 absolute z-10">
       {/* 1. Logo stays on the left */}
       <div className="shrink-0">
-        <Image src="/bookit.png" width={80} height={20} alt="logo" />
+        <Image src="/bookit-white.png" width={90} height={40} alt="logo" />
       </div>
 
       {/* 2. Links - "mx-auto" pushes everything else away to center these */}
-      <nav className="flex items-center gap-7 mx-auto">
-        <Link href="/" className="text-lg text-cyan-50">
+      <nav className="flex items-center mx-auto  rounded-xl bg-white">
+        <Link href="/" className={navItem}>
           Home
         </Link>
-        <Link href="/" className="text-xl font-bold">
+        <Link href="/" className={navItem}>
           Hotel & Homes
         </Link>
-        <Link href="/" className="text-xl font-bold">
+        <Link href="/" className={navItem}>
           Real Estate
         </Link>
-        <Link href="/" className="text-xl font-bold">
+        <Link href="/" className={navItem}>
           Articles
         </Link>
       </nav>
 
       {/* 3. Button - stays on the right */}
-      <div className="shrink-0">
-        {session?.user ? <LogoutButton /> : <Button>Get Started</Button>}
+      <div className="shrink-0 flex gap-2 pr-6">
+        {session?.user ? (
+          <DropdownMenuAvatar avatarsize="lg" />
+        ) : (
+          <Button>Get Started</Button>
+        )}
       </div>
     </header>
   );
