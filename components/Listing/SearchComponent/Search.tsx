0307955f65131d6c11a4fa297291c82cbdfa0ae8
Jarmen Cachero <cachero.jarmen.bscs2022@gmail.com>
2026-05-27 23:03:47 +0800

    feat: search functionality now working on search client component

--- components/Listing/SearchComponent/Search.tsx
+++ components/Listing/SearchComponent/Search.tsx
@@ -1,12 +1,29 @@
+"use client";
 import React from "react";
 import { Label } from "../../ui/label";
 import { DropdownMenuWhere } from "./DropdownWhere";
-import { getAllCities } from "@/app/lib/city";
 import { DropdownMenuWhen } from "./DropdownWhen";
 import { DropdownMenuWho } from "./DropdownWho";
+import { City } from "@prisma/client";
+import { useSearchStore } from "@/store/useSearchStore";
+import { useRouter } from "next/navigation";
 
-const Search = async () => {
-  const cities = await getAllCities();
+const Search = ({ cities }: { cities: City[] }) => {
+  const router = useRouter();
+  const { city, dateRange, adults, children } = useSearchStore();
+  const handleSearch = () => {
+    if (!city) return;
+
+    const params = new URLSearchParams({
+      city: city.name,
+      from: dateRange?.from?.toISOString() ?? "",
+      to: dateRange?.to?.toISOString() ?? "",
+      adults: adults.toString(),
+      children: children.toString(),
+    });
+
+    router.push(`/listing?${params.toString()}`);
+  };
   return (
     <div className="bg-neutral-100 rounded-xl p-4 grid grid-cols-1 sm:grid-cols-10 gap-3 max-w-4xl mx-auto">
       <div className="sm:col-span-3 grid gap-2">
@@ -23,7 +40,10 @@ const Search = async () => {
       </div>
       <div className="sm:col-span-1 grid gap-2">
         <Label className="text-black hidden sm:block"></Label>
-        <button className="bg-black text-white w-full py-2 lg:px-4 px-2 rounded-lg text-sm">
+        <button
+          onClick={handleSearch}
+          className="bg-black text-white w-full py-2 lg:px-4 px-2 rounded-lg text-sm"
+        >
           Search
         </button>
       </div>
